6f13d0670e4aa36fa1b40d24e2bdb33958fb7910
Kevin S. Clarke <ksclarke@ksclarke.io>
2018-04-05 18:04:37 -0400

    Upgrade system before running Ansible

--- scripts/setup-base.sh
+++ scripts/setup-base.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Upgrade the system
+apt-get -y update
+apt-get -y upgrade
+
 # Setup Ansible
 apt-get -y install software-properties-common python-software-properties
 apt-add-repository -y ppa:ansible/ansible
