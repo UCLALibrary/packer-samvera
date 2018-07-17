ce848fa70da19ef394d619043273a85f0d29f8c7
Kevin S. Clarke <ksclarke@ksclarke.io>
2018-07-17 16:53:06 -0400

    Set system locale and install nodejs

--- scripts/setup-base.sh
+++ scripts/setup-base.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Set the system language
+update-locale LANG="en_US.UTF-8" LANGUAGE="en_US"
+
 # Upgrade the system
 apt-get -y update
 apt-get -y upgrade
@@ -12,4 +15,4 @@ apt-get -y update
 apt-get -y install -f ansible
 
 # Setup some basics
-apt-get -y install nano htop git nmap curl wget tmux lynx
+apt-get -y install nano htop git nmap curl wget tmux lynx nodejs
