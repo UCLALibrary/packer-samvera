7c5f847856a0e147671bf12471548c2e0e21dfa7
Kevin S. Clarke <ksclarke@ksclarke.io>
2018-07-20 18:49:49 -0400

    Add test and development dotenv files

--- scripts/setup-hyrax.sh
+++ scripts/setup-hyrax.sh
@@ -4,6 +4,20 @@
 # This is stuff that mostly really belongs elsewhere (?)
 ##
 
+createDotEnv() {
+  touch "/opt/${1}/shared/.env.${2}"
+  chown -R deploy:deploy "/opt/${1}/"
+
+  echo "PROJECT_NAME=${1}" >> "/opt/${1}/shared/.env.${2}"
+  echo "DATABASE_NAME=${3}" >> "/opt/${1}/shared/.env.${2}"
+  echo "DATABASE_USERNAME=${4}" >> "/opt/${1}/shared/.env.{2}"
+  echo "DATABASE_PASSWORD=${5}" >> "/opt/${1}/shared/.env.{2}"
+  echo "DATABASE_POOL_SIZE=25" >> "/opt/${1}/shared/.env.${2}"
+  echo "GEONAMES_USERNAME=${6}" >> "/opt/${1}/shared/.env.${2}"
+  echo "RAILS_SERVE_STATIC_FILES=true" >> "/opt/${1}/shared/.env.${2}"
+  echo "SIDEKIQ_WORKERS=7" >> "/opt/${1}/shared/.env.${2}"
+}
+
 # Ubuntu ISO doesn't have this by default
 gem install nokogiri -v '1.8.2'
 
@@ -15,21 +29,16 @@ echo "export SECRET_KEY_BASE=$SECRET_KEY_BASE" >> /etc/environment
 echo "export DB_USERNAME=$DB_USERNAME" >> /etc/environment
 echo "export DB_PASSWORD=$DB_PASSWORD" >> /etc/environment
 echo "export DB_NAME=$DB_NAME" >> /etc/environment
+echo "export DATABASE_USERNAME=$DB_USERNAME" >> /etc/environment
+echo "export DATABASE_PASSWORD=$DB_PASSWORD" >> /etc/environment
+echo "export DATABASE_NAME=$DB_NAME" >> /etc/environment
 echo "export BRANCH=$GITHUB_BRANCH" >> /etc/environment
 echo "export PROJECT_NAME=$PROJECT_NAME" >> /etc/environment
 echo "export PROJECT_OWNER=$PROJECT_OWNER" >> /etc/environment
 echo "export GEONAMES_USERNAME=$GEONAMES_USERNAME" >> /etc/environment
 
-# I'm not using dotenv but something in the build expects this so we fake it...
-mkdir -p /opt/${PROJECT_NAME}/shared
-touch /opt/${PROJECT_NAME}/shared/.env.production
-chown -R deploy:deploy /opt/${PROJECT_NAME}/
+mkdir -p "/opt/${PROJECT_NAME}/shared"
 
-echo "PROJECT_NAME=${PROJECT_NAME}" >> /opt/${PROJECT_NAME}/shared/.env.production
-echo "DATABASE_NAME=${DB_NAME}" >> /opt/${PROJECT_NAME}/shared/.env.production
-echo "DATABASE_USERNAME=${DB_USERNAME}" >> /opt/${PROJECT_NAME}/shared/.env.production
-echo "DATABASE_PASSWORD=${DB_PASSWORD}" >> /opt/${PROJECT_NAME}/shared/.env.production
-echo "DATABASE_POOL_SIZE=25" >> /opt/${PROJECT_NAME}/shared/.env.production
-echo "GEONAMES_USERNAME=${GEONAMES_USERNAME}" >> /opt/${PROJECT_NAME}/shared/.env.production
-echo "RAILS_SERVE_STATIC_FILES=true" >> /opt/${PROJECT_NAME}/shared/.env.production
-echo "SIDEKIQ_WORKERS=7" >> /opt/${PROJECT_NAME}/shared/.env.production
+createDotEnv "$PROJECT_NAME" "production" "$DB_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME"
+createDotEnv "$PROJECT_NAME" "test" "$DB_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME"
+createDotEnv "$PROJECT_NAME" "development" "$DB_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME"
