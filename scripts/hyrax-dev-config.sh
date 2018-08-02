35c7073995e7d4ad07e613f4d63e3a2f03699e0b
Kevin S. Clarke <ksclarke@ksclarke.io>
2018-08-01 23:42:13 -0400

    Add ADMIN_PASSWORD [fixes #24] and fix .env [fixes #22]

--- scripts/hyrax-dev-config.sh
+++ scripts/hyrax-dev-config.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
-# Create a database user for the system user (in our case 'vagrant')
-sudo -u postgres PGPASSWORD="$ROOT_DB_PASSWORD" psql -c "CREATE USER vagrant WITH LOGIN CREATEDB PASSWORD 'vagrant';"
+# Alter our system database user so that it can create databases
+sudo -u postgres PGPASSWORD="$ROOT_DB_PASSWORD" psql -c "ALTER USER vagrant WITH CREATEDB;"
 
-# Install databases specific to a developer's workstation
+# Link to .env file for the system user's code base
+sudo ln -s /opt/${PROJECT_NAME}/shared/.env.development /home/vagrant/${PROJECT_NAME}/.env.development
+sudo ln -s /opt/${PROJECT_NAME}/shared/.env.production /home/vagrant/${PROJECT_NAME}/.env.production
+sudo ln -s /opt/${PROJECT_NAME}/shared/.env.test /home/vagrant/${PROJECT_NAME}/.env.test
+
+# Install test database; Ansible has installed the other database
 cd "/home/vagrant/${PROJECT_NAME}"
-sudo -u vagrant bundle exec rake db:setup
+RAILS_ENV=test bundle exec rake db:setup
 
 # Setup an alias for `bundle exec`
 echo -e "alias be=\"bundle exec\"\n" >> /home/vagrant/.bash_aliases
--- scripts/setup-hyrax.sh
+++ scripts/setup-hyrax.sh
@@ -5,17 +5,19 @@
 ##
 
 createDotEnv() {
-  touch "/opt/${1}/shared/.env.${2}"
-  chown -R deploy:deploy "/opt/${1}/"
+  ENV_FILE="/opt/${1}/shared/.env.${2}"
 
-  echo "PROJECT_NAME=${1}" >> "/opt/${1}/shared/.env.${2}"
-  echo "DATABASE_NAME=${3}" >> "/opt/${1}/shared/.env.${2}"
-  echo "DATABASE_USERNAME=${4}" >> "/opt/${1}/shared/.env.${2}"
-  echo "DATABASE_PASSWORD=${5}" >> "/opt/${1}/shared/.env.${2}"
-  echo "DATABASE_POOL_SIZE=25" >> "/opt/${1}/shared/.env.${2}"
-  echo "GEONAMES_USERNAME=${6}" >> "/opt/${1}/shared/.env.${2}"
-  echo "RAILS_SERVE_STATIC_FILES=true" >> "/opt/${1}/shared/.env.${2}"
-  echo "SIDEKIQ_WORKERS=7" >> "/opt/${1}/shared/.env.${2}"
+  touch "$ENV_FILE"
+
+  echo "PROJECT_NAME=${1}" >> "$ENV_FILE"
+  echo "DATABASE_NAME=${3}" >> "$ENV_FILE"
+  echo "DATABASE_USERNAME=${4}" >> "$ENV_FILE"
+  echo "DATABASE_PASSWORD=${5}" >> "$ENV_FILE"
+  echo "DATABASE_POOL_SIZE=25" >> "$ENV_FILE"
+  echo "GEONAMES_USERNAME=${6}" >> "$ENV_FILE"
+  echo "RAILS_SERVE_STATIC_FILES=true" >> "$ENV_FILE"
+  echo "SIDEKIQ_WORKERS=7" >> "$ENV_FILE"
+  echo "ADMIN_PASSWORD=${7}" >> "$ENV_FILE"
 }
 
 # Ubuntu ISO doesn't have this by default
@@ -33,6 +35,11 @@ echo "export GEONAMES_USERNAME=$GEONAMES_USERNAME" >> /etc/environment
 
 mkdir -p "/opt/${PROJECT_NAME}/shared"
 
-createDotEnv "$PROJECT_NAME" "production" "$DB_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME"
-createDotEnv "$PROJECT_NAME" "test" "${DB_NAME}_test" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME"
-createDotEnv "$PROJECT_NAME" "development" "${DB_NAME}_development" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME"
+createDotEnv "$PROJECT_NAME" "production" "$DB_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME" "$ADMIN_PASSWORD"
+createDotEnv "$PROJECT_NAME" "test" "${DB_NAME}_test" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME" "$ADMIN_PASSWORD"
+createDotEnv "$PROJECT_NAME" "development" "$DB_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME" "$ADMIN_PASSWORD"
+
+# Trying making the development the same as production since we don't have production on our dev box
+#createDotEnv "$PROJECT_NAME" "development" "${DB_NAME}_development" "$DB_USERNAME" "$DB_PASSWORD" "$GEONAMES_USERNAME" "$ADMIN_PASSWORD"
+
+chown -R deploy:deploy "/opt/${PROJECT_NAME}/"
